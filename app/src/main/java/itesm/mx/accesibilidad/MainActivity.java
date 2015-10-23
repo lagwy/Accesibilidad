af40f468af89757c7d555fba83c375565cea5978
Daniel <danlaster@hotmail.com>
2015-10-23 17:55:25 -0500

    Daniel commit

--- app/src/main/java/itesm/mx/accesibilidad/MainActivity.java
+++ app/src/main/java/itesm/mx/accesibilidad/MainActivity.java
@@ -8,11 +8,16 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    //Hola
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
