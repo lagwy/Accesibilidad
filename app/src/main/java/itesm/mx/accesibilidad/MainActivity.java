832149a14f68b7e649d2677c955c9fb1b4088dcb
Luis Ángel Martínez <lagwyf@gmail.com>
2015-11-27 20:08:19 -0600

    Abrir la aplicación únicamente cuando haya conexión WiFi

--- app/src/main/java/itesm/mx/accesibilidad/MainActivity.java
+++ app/src/main/java/itesm/mx/accesibilidad/MainActivity.java
@@ -1,6 +1,9 @@
 package itesm.mx.accesibilidad;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -9,11 +12,12 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    Context context;
     //Hola mundo
     /*
         Todo: Actualizar la actividad VistaEdificio para que muestre las descripciones correctas.
@@ -29,7 +33,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
 
+        // Revisar la conexión de internet, tiene que ser por wifi, si no se lanza un toast y se cierra la aplicación
+        ConnectivityManager networkManager = (ConnectivityManager) context.getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = networkManager.getActiveNetworkInfo();
+        NetworkInfo wifi = networkManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (wifi.isAvailable() && wifi.isConnected()) {
+            // La aplicación hacer lo demás
+        }else {
+            // Desplegar un mensaje de que no hay conexión a internet o no es por wifi
+            Toast toast = Toast.makeText(getApplicationContext(), "No estas utilizando una conexión WiFi\n" +
+                    "conectate a una y vuelve a intentar.", Toast.LENGTH_SHORT);
+            toast.show();
+            // Terminar la aplicacion
+            finish();
+        }
 
         final Button rutaBttn = (Button) findViewById(R.id.rutaBttn);
         final Button edificioBttn = (Button) findViewById(R.id.ediBttn);
