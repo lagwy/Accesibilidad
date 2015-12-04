77ca791d9f2c9360e8f9e8fc1d44c44207c14e2c
Luis Ángel Martínez <lagwyf@gmail.com>
2015-12-03 18:30:13 -0600

    Licencia de Java, reubicación de GPL a la parte de abajo de los documentos .java

--- app/src/main/java/itesm/mx/accesibilidad/VistaEdificio.java
+++ app/src/main/java/itesm/mx/accesibilidad/VistaEdificio.java
@@ -1,32 +1,18 @@
 /*
-Accesibilidad - Proyecto de la materia de Desarrollo de Aplicaciones Móviles
-        Copyright (C) 2015 - ITESM
-
-        This program is free software: you can redistribute it and/or modify
-        it under the terms of the GNU General Public License as published by
-        the Free Software Foundation, either version 3 of the License, or
-        (at your option) any later version.
-
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        GNU General Public License for more details.
-
-        You should have received a copy of the GNU General Public License
-        along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-        Authors:
-
-        ITESM representatives
-        Ing. Martha Sordia Salinas <msordia@itesm.mx>
-        Ing. Mario de la Fuente <mario.delafuente@itesm.mx>
-
-        ITESM students
-        Luis Angel Martinez Garcia <a00813485@itesm.mx>
-        Daniel Garcia Mena <a00813719@itesm.mx>
-        Jorge Luis Marquez Sanchez <a01139543@itesm.mx>
-*/
+ * Copyright (C) 2015 ITESM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package itesm.mx.accesibilidad;
 
@@ -136,3 +122,33 @@ public class VistaEdificio extends AppCompatActivity {
 
     } // Termina clase de carga de imagen
 }
+
+/*
+Accesibilidad - Proyecto de la materia de Desarrollo de Aplicaciones Móviles
+        Copyright (C) 2015 - ITESM
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+        Authors:
+
+        ITESM representatives
+        Ing. Martha Sordia Salinas <msordia@itesm.mx>
+        Ing. Mario de la Fuente <mario.delafuente@itesm.mx>
+
+        ITESM students
+        Luis Angel Martinez Garcia <a00813485@itesm.mx>
+        Daniel Garcia Mena <a00813719@itesm.mx>
+        Jorge Luis Marquez Sanchez <a01139543@itesm.mx>
+*/
